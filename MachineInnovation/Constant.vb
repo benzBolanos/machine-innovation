ef5e355868bb7d22237c5136aa19dc62683cfacc
Benison Bolanos <ben@offshorly.com>
2018-02-18 15:07:32 +0800

    added serverstring in constancts

--- MachineInnovation/Constant.vb
+++ MachineInnovation/Constant.vb
@@ -1,6 +1,8 @@
 ﻿Imports MySql.Data.MySqlClient
 Module Constant
     Public SqlConnection As MySqlConnection = New MySqlConnection
+    Public ServerString As String = "host=localhost;user=root;password=;database=db_machine_innovation;"
 
     Public USERNAME As String = ""
+
 End Module
